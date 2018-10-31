de896dbdcb5a82e12904f1775e76cf9db0fbf108
Shubham Dalvi <shubham.dalvi97@gmail.com>
2018-11-01 00:46:28 +0530

    Add files via upload

---
+++ Trees/MinimalTree.java
@@ -0,0 +1,56 @@
+class BSTNode
+{
+	int data;
+	BSTNode left;
+	BSTNode right;
+	
+	BSTNode(int d)
+	{
+		data = d;
+		left = null;
+		right = null;
+	}
+	int getData()
+	{
+		return data;
+	}
+}
+class MinimalTree
+{
+	public static void main(String args[])
+	{		
+		
+		/*
+		
+					
+					10
+ 				  /	   \
+				5		20
+			  /   \		/ \
+			2	   8  15  30		
+		
+		
+		*/
+		
+		BSTNode root;
+		int sortedArray[] = {1,2,3,4,5,6,7};
+		root = createMinimalBST(sortedArray);
+		System.out.println("Root: "+root.getData()); // 4
+			
+	}
+	static BSTNode createMinimalBST(int[] arr)
+	{
+		return createMinimalBST(arr, 0, arr.length-1);
+	}
+	static BSTNode createMinimalBST(int[] arr, int start, int end)
+	{
+		if(start > end)
+			return null;
+		int mid = (start + end)/2;
+		
+		BSTNode node = new BSTNode(arr[mid]);
+		node.left = createMinimalBST(arr,start,mid-1);
+		node.right = createMinimalBST(arr,mid+1,end);
+		return node;
+	}		
+}
