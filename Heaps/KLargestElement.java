abc4f84d9defef6d2fd57a9a97db93bb3d5e93df
= <=>
2019-10-13 23:50:23 +0530

    optimized space complexity to k

--- Heaps/KLargestElement.java
+++ Heaps/KLargestElement.java
@@ -1,4 +1,3 @@
-import java.util.Comparator;
 import java.util.PriorityQueue;
 
 /**
@@ -8,31 +7,21 @@ import java.util.PriorityQueue;
 public class KLargestElement {
 
     public static void main(String[] args) {
-        int arr[] = {3,2,3,1,2,4,5,5,6};
-        int k = 4;
+        int arr[] = {2,4,5,1};
+        int k = 3;
         int res = findKthLargestElement(arr, k);
         System.err.println("Kth largest element = "+res);
     }
     public static int findKthLargestElement(int[] arr, int k){
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                if(o1 > o2)
-                    return -1;
-                else if (o1 < o2)
-                    return 1;
-                else
-                    return 0;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
+
+        for (int i : arr) {
+            minHeap.add(i);
+            if(minHeap.size() > k){
+                minHeap.poll();
             }
-        });
-
-        for (int i = 0; i < arr.length; i++) {
-            maxHeap.add(arr[i]);
         }
 
-        for (int i = 0; i < k-1; i++) {
-            maxHeap.poll();
-        }
-        return maxHeap.peek();
+        return minHeap.poll();
     }
 }
--- Heaps/KSmallestElement.java
+++ Heaps/KSmallestElement.java
@@ -1,3 +1,4 @@
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 /**
@@ -9,20 +10,29 @@ public class KSmallestElement {
     public static void main(String[] args) {
         int arr[] = {3,2,3,1,2,4,5,5,6};
         int k = 4;
-        int res = findKthLargestElement(arr, k);
+        int res = findKthSmallestestElement(arr, k);
         System.err.println("Kth smallest element = "+res);
     }
-    public static int findKthLargestElement(int[] arr, int k){
-        
-        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
+    public static int findKthSmallestestElement(int[] arr, int k){
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if(o1 > o2)
+                    return -1;
+                else if (o1 < o2)
+                    return 1;
+                else
+                    return 0;
+            }
+        });
 
-        for (int i = 0; i < arr.length; i++) {
-            minHeap.add(arr[i]);
+        for (int i : arr) {
+            maxHeap.add(i);
+            if (maxHeap.size() > k) {
+                maxHeap.poll();
+            }
         }
 
-        for (int i = 0; i < k-1; i++) {
-            minHeap.poll();
-        }
-        return minHeap.peek();
+        return maxHeap.poll();
     }
 }
