d6ce02a114e93627e51bea65e88d2b49e94a6b50
saintlyzero <shubham.dalvi97@gmail.com>
2019-12-16 23:54:56 +0530

    Coin change recurssive

--- DynamicProgramming/coin_change.py
+++ DynamicProgramming/coin_change.py
@@ -1,4 +1,4 @@
-def count_possibility_recurssive(coins:list, coin_number:int, target:int):
+def count_combinations_recurssive(coins:list, coin_number:int, target:int):
     
     # If target is 0 then there is 1 
     # solution : do not include any coin
@@ -11,13 +11,14 @@ def count_possibility_recurssive(coins:list, coin_number:int, target:int):
     if target < 0:
         return 0
     
-    ways_without_current_coin = count_possibility_recurssive(coins, coin_number-1, target)
-    ways_with_current_coin = count_possibility_recurssive(coins, coin_number, target - coins[coin_number])
+    ways_without_current_coin = count_combinations_recurssive(coins, coin_number-1, target)
+    ways_with_current_coin = count_combinations_recurssive(coins, coin_number, target - coins[coin_number])
+
     return ways_without_current_coin + ways_with_current_coin
 
 if __name__ == "__main__":
     coins = [1,2,3]
     coin_number = len(coins) - 1
     target = 4
-    combinations = count_possibility_recurssive(coins, coin_number, target)
+    combinations = count_combinations_recurssive(coins, coin_number, target)
     print(combinations)
