de3a6d4e189fea85b7a7f29ab26fa17a01e11804
saintlyzero <shubham.dalvi97@gmail.com>
2019-12-17 18:59:16 +0530

    Add coin change dp solution

--- DynamicProgramming/coin_change.py
+++ DynamicProgramming/coin_change.py
@@ -16,9 +16,27 @@ def count_combinations_recurssive(coins:list, coin_number:int, target:int):
 
     return ways_without_current_coin + ways_with_current_coin
 
+
+
+def count_combinations_dp(coins:list, total_coins:int, target:int):
+    
+    table = [0 for i in range(target+1)]
+    table[0] = 1
+
+    for coin in coins:
+        for j in range(coin,target+1):
+            table[j] += table[j-coin]
+    
+    return table[target]
+
+
 if __name__ == "__main__":
     coins = [1,2,3]
     coin_number = len(coins) - 1
-    target = 4
-    combinations = count_combinations_recurssive(coins, coin_number, target)
+    target = 10
+    # combinations = count_combinations_recurssive(coins, coin_number, target)
+    # print(combinations)
+    
+    
+    combinations = count_combinations_dp(coins, coin_number, target)
     print(combinations)
