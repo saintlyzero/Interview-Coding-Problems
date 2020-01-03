7d9afe79c47030b3818be7fa2ee0eaa9771e5d3d
saintlyzero <shubham.dalvi97@gmail.com>
2020-01-03 12:06:41 +0530

    Add 2D array total possible paths

--- DynamicProgramming/array_paths.py
+++ DynamicProgramming/array_paths.py
@@ -11,12 +11,22 @@ def number_of_ways(n: int, m:int):
 
 def number_of_ways_to_XY(n: int, m:int, numberOfWays:list):
     
+    # If 1st row or 1st column,
+    # then there's just 1 way to reach
     if n == 0 or m == 0:
         return 1
 
     if(numberOfWays[n][m] == 0):
+
+        # Number of paths to reach the Top-
+        # cell of the current cell 
         waysToTop =  number_of_ways_to_XY(n-1, m, numberOfWays)
+
+        # Number of paths to reach the left-
+        # cell of the current cell
         waysToLeft = number_of_ways_to_XY(n, m-1, numberOfWays)
+
+        # Total number of ways to reach current cell
         numberOfWays[n][m] = waysToLeft + waysToTop
     
     return numberOfWays[n][m]
